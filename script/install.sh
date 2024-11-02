854fca818b0a96d14cb41840faab381008a51133
Soundjata <jm24.973@gmail.com>
2024-11-02 15:41:55 +0100

    deleted:    script/boomi-installer-menu.sh 	new file:   script/install.sh

---
+++ script/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get -qq --yes update
+sudo apt-get -qq --yes install git unzip zip sudo jq
+
+if [ -d "/etc/Boomi-Installer" ]; then sudo rm -R /etc/Boomi-Installer; fi
+sudo mkdir /etc/Boomi-Installer
+sudo git clone https://github.com/Soundjata/Boomi-Installer.git /etc/Boomi-Installer
+sudo chmod +x /etc/Boomi-Installer/script/menu.sh
+sudo chown $ID:$ID -R /etc/Boomi-Installer
